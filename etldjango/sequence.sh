41e739f9b97573587db7986bcdc2f4ff77ce0e7a
DavidCastilloAlvarado <dcastilloa@uni.pe>
2021-03-30 21:43:03 -0500

    Readme updated - commands doc

--- etldjango/sequence.sh
+++
@@ -1,7 +0,0 @@
-python manage.py worker_extractor
-python manage.py worker_getbucket
-python manage.py worker_t_sinadef no
-python manage.py worker_t_uci yes
-python manage.py worker_posit no
-python manage.py worker_rt db
-python manage.py worker_mov
