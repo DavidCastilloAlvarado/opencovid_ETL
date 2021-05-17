1170e1c34ddc9330816c562bdec7784b404c8939
David Castillo <dcastilloa@uni.pe>
2021-05-16 23:37:02 -0500

    deploy fixed

--- etldjango/deploy.sh
+++ etldjango/deploy.sh
@@ -1,7 +1,7 @@
 # Setup principal variables
 gcloud auth list
 gcloud config list project
-_PROJECT_ID=$(gcloud config get-value core/project)
+PROJECT_ID=$(gcloud config get-value core/project)
 REGION=us-central1
 
 # Create Bucket
@@ -10,7 +10,7 @@ REGION=us-central1
 
 # Create .env secrets by secretmanager
 gcloud secrets create application_settings_etl --data-file .env
-export PROJECTNUM=$(gcloud projects describe ${_PROJECT_ID} --format 'value(projectNumber)')
+export PROJECTNUM=$(gcloud projects describe ${PROJECT_ID} --format 'value(projectNumber)')
 export CLOUDRUN=${PROJECTNUM}-compute@developer.gserviceaccount.com
 rm .env
 
@@ -20,28 +20,28 @@ gcloud secrets add-iam-policy-binding application_settings_etl \
 gcloud secrets versions list application_settings_etl
 
 # Allow access to components
-export PROJECTNUM=$(gcloud projects describe ${_PROJECT_ID} --format 'value(projectNumber)')
+export PROJECTNUM=$(gcloud projects describe ${PROJECT_ID} --format 'value(projectNumber)')
 export CLOUDBUILD=${PROJECTNUM}@cloudbuild.gserviceaccount.com
 
 # Build  Docker container
-# gcloud builds submit --tag gcr.io/$_PROJECT_ID/opencovid2-etl
-# gcloud container images list
+gcloud builds submit --tag gcr.io/$PROJECT_ID/opencovid2-etl
+gcloud container images list
 
 # Past secrets to the Docket container
 gcloud secrets add-iam-policy-binding application_settings_etl \
   --member serviceAccount:${CLOUDBUILD} --role roles/secretmanager.secretAccessor
 
-gcloud projects add-iam-policy-binding ${_PROJECT_ID} \
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --member serviceAccount:${CLOUDBUILD} --role roles/cloudsql.client
 
 # Run Migrations
-gcloud builds submit --config cloudmigrate.yaml 
+gcloud builds submit --config cloudbuild.yaml --substitutions _PROJECT_ID=${PROJECT_ID}
 
 # Django on Cloud Run
 gcloud beta run deploy opencovid2-etl --platform managed --region $REGION \
   --cpu 2 \
   --timeout 3600 \
   --memory 4096 \
-  --image gcr.io/$_PROJECT_ID/opencovid2-etl \
-  --add-cloudsql-instances ${_PROJECT_ID}:${REGION}:opencovid-db \
+  --image gcr.io/$PROJECT_ID/opencovid2-etl \
+  --add-cloudsql-instances ${PROJECT_ID}:${REGION}:opencovid-db \
   --allow-unauthenticated
